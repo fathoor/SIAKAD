b30dc92d4f5be7de256920bbd74d32b1db8b15b9
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-15 12:25:32 +0700

    updated homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,36 +7,35 @@
 {{-- Content --}}
 @section('content')
 
-<div class="container">
-    {{-- Header --}}
-    {{-- @include('components.header') --}}
-
-    {{-- Content --}}
-    <div class="content">
-        <div class="form-box">
-            <a href="/" title="SIAKAD ITS">
-                <img src="/img/siakad_putih.png" alt="SIAKAD ITS" class="img-fluid">
-            </a>
-            <div>
-                <form>
-                    @csrf
-                    
-                    <div class="form-floating mt-4">
-                        <input type="text" id="username" class="form-control shadow-p bg-transparent text-white" placeholder="Username" required>
-                        <label for="username">Username</label>
-                    </div>
-                    <div class="form-floating mt-3">
-                        <input type="password" id="password" class="form-control shadow-p bg-transparent text-white" placeholder="Password" required>
-                        <label for="password">Password</label>
-                    </div>
-                    <div class="d-grid mt-3">
-                        <button class="btn btn-light btn-lg shadow-p" type="submit">Login</button>
-                    </div>
-                </form>
+<body class="bg-blue">
+    <div class="container bg-blue">
+        {{-- Content --}}
+        <div class="content">
+            <div class="form">
+                <a href="/dashboard" title="SIAKAD ITS">
+                    <img src="/img/siakad_putih.png" alt="SIAKAD ITS" class="img-fluid">
+                </a>
+                <div>
+                    <form>
+                        @csrf
+                        
+                        <div class="form-floating mt-4">
+                            <input type="text" id="username" class="form-control shadow-p bg-transparent text-white" placeholder="Username" required>
+                            <label for="username">Username</label>
+                        </div>
+                        <div class="form-floating mt-3">
+                            <input type="password" id="password" class="form-control shadow-p bg-transparent text-white" placeholder="Password" required>
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="d-grid mt-3">
+                            <button class="btn btn-light btn-lg shadow-p" type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+    
+        {{-- Footer --}}
+        @include('components.footer')
     </div>
-
-    {{-- Footer --}}
-    @include('components.footer')
-</div>
+</body>
