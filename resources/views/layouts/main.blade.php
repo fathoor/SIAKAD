91ca58b27c3d8f8f9fcdbab471e1d8afc4ee91bb
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-16 05:45:21 +0700

    updated main layout
    Changes:
    + jQuery CDN
    + yield 'bg' untuk class body
    + script JS

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
+    {{-- jQuery --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     {{-- CSS --}}
     <link rel="stylesheet" href="/css/app.css">
 
@@ -19,5 +22,10 @@
     {{-- Title Halaman --}}
     <title>SIAKAD ITS - @yield('title')</title>
 </head>
-@yield('content') 
+<body class="@yield('bg')">
+    @yield('content')
+
+    {{-- JS --}}
+    <script src="/js/app.js"></script>
+</body>
 </html>
