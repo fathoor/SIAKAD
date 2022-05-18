36e8dbbd981cf6f1420e7f08621a1de4326ff584
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-18 10:35:24 +0700

    updated footer
    Changes:
    + added "Rancang Bangun Perangkat Lunak [C]" text
    + fixed navbar margin when collapsed
    + added flex-direction to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,9 @@
 {{-- Footer --}}
 <footer class="footer">
-    <div class="d-flex align-items-center justify-content-center mx-auto">
+    <div class="d-flex flex-row align-items-center justify-content-center mx-auto">
         <span>&copy; 2022</span>
+    </div>
+    <div class="d-flex flex-row align-items-center justify-content-center mx-auto">
+        <span>Rancang Bangun Perangkat Lunak [C]</span>
     </div>
 </footer>
