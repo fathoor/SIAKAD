d8eb1e94f4d301ba4aaa90fc5bceb460c3df3013
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-19 10:09:06 +0700

    added mahasiswa migration
    Changes:
    - removed default laravel migrations
    
    + added mahasiswa migration

--- database/migrations/2022_05_19_010036_create_mahasiswa_table.php
+++ database/migrations/2022_05_19_010036_create_mahasiswa_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mahasiswa', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('namaMahasiswa');
+            $table->string('NRP')->unique();
             $table->string('password');
+            $table->string('NIK');
+            $table->string('tempatLahir');
+            $table->date('tanggalLahir');
+            $table->string('nomorTelp');
+            $table->string('email');
+            $table->integer('tahunMasuk');
+            $table->string('alamat');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mahasiswa');
     }
 };
