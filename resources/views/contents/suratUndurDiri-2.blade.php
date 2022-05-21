f18a88910624e644f60eafe793ba545c59d160fd
Ernand29 <ernando.taufiq29@gmail.com>
2022-05-21 21:24:59 +0700

    Edited Kurikulum

--- resources/views/contents/suratUndurDiri-2.blade.php
+++ resources/views/contents/suratUndurDiri-2.blade.php
@@ -5,7 +5,7 @@
 {{-- Content --}}
 @section('main')
 
-    <div class="container">
+    <div class="container" style="justify-content: start">
         {{-- Content --}}
         <main>
             {{-- /view/contents/ --}}
@@ -19,18 +19,18 @@
                     <div class="formBox">
                         <form class="form-inline" id="input" action="">
                             @csrf
-                            <div class="form-group row mb-3">
-                                <label for="NRP" class="col-3 col-form-label">NRP</label>
+                            <div class="form-group row mb-2">
+                                <label for="NRP" class="col-2 col-form-label">NRP</label>
                                 <label for="NRP" class="col-1 col-form-label">:</label>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <input type="text" id="NRP" name="NRP" class="form-control bg-transparent border-white"
                                         value="{{ auth()->user()->NRP }}" disabled>
                                 </div>
                             </div>
-                            <div class="form-group row mb-3">
-                                <label for="nama" class="col-3 col-form-label">Nama</label>
+                            <div class="form-group row mb-2">
+                                <label for="nama" class="col-2 col-form-label">Nama</label>
                                 <label for="nama" class="col-1 col-form-label">:</label>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <input type="text" id="nama" name="nama"
                                         class="form-control bg-transparent border-white"
                                         value="{{ auth()->user()->nama }}" disabled>
