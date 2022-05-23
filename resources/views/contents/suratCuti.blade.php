f867c692e08e96ab1a552b7a645760dfcfe5a055
ceciliamelva <ceciliamelvaa@gmail.com>
2022-05-23 10:33:03 +0700

    First Upload Surat Cuti
    + added surat cuti dashboard page

--- resources/views/contents/suratCuti.blade.php
+++ resources/views/contents/suratCuti.blade.php
@@ -1,6 +1,6 @@
 @extends('dashboard')
 
-@section('title', 'Surat Cuti')
+@section('title', 'Transkrip')
 
 {{-- Content --}}
 @section('main')
@@ -11,9 +11,53 @@
         {{-- /view/contents/ --}}
         <div class="content">
             {{-- Isi disini --}}
-        
+            <div class="container text-start">
+                <h2 class="text-start text-dark mb-4">
+                    <strong> Surat Cuti </strong>
+                </h2>
+
+                <div class="formBox">
+                    <form class="form-inline" id="input" action="">
+                        @csrf
+                        <div class="form-group row mb-3">
+                            <label for="NRP" class="col-3 col-form-label">NRP</label>
+                            <label for="NRP" class="col-1 col-form-label">:</label>
+                            <div class="col-8">
+                                <input type="text" id="NRP" name="NRP" class="form-control bg-transparent border-white" value="{{ auth()->user()->NRP }}" disabled>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-3">
+                            <label for="namaMahasiswa" class="col-3 col-form-label">Nama</label>
+                            <label for="namaMahasiswa" class="col-1 col-form-label">:</label>
+                            <div class="col-8">
+                                <input type="text" id="namaMahasiswa" name="namaMahasiswa" class="form-control bg-transparent border-white" value="{{ auth()->user()->namaMahasiswa }}" disabled>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <a href="/contents.suratMahasiswa" class="btn btn-primary"> + Ajukan Surat</a>
+                <br/>
+                <br/>
+
+                <table class="table table-striped">
+                    <tr>
+                        <th>No</th>
+                        <th>Periode</th>
+                        <th>Jumlah Smt Cuti</th>
+                        <th>Diajukan</th>
+                        <th>Alasan</th>
+                        <th>Status</th>
+                        <th>Cetak</th>
+                    </tr>
+
+
+                </table>
+
+            </div>
         </div>
     </main>
 </div>
 
 @endsection
+
