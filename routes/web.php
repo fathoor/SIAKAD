4a59d1e707cbec353ecbc88548145d592281a2fe
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-23 07:48:51 +0700

    Updated Routes
    Changes:
    + Grouping routes based on middleware
    + Redirected 'mahasiswa' role to /dashboard
    + Redirected 'dosen' role to /dashboard/dosen

--- routes/web.php
+++ routes/web.php
@@ -15,63 +15,73 @@ use App\Http\Controllers\DashboardController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 // Homepage Route
 Route::get('/', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
-// Dashboard Route
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
-
-// Akademik Route
-Route::get('/frs', function () {
-    return view('contents.frs');
-});
-
-Route::get('/transkrip', function () {
-    return view('contents.transkrip');
-});
-
-Route::get('/kurikulum', function () {
-    return view('contents.kurikulum');
-});
-
-// Finansial Route
-Route::get('/ukt', function () {
-    return view('contents.ukt');
-});
-
-Route::get('/ukt-2', function () {
-    return view('contents.ukt-2');
-});
-
-// Layanan Route
-Route::get('/suratundurdiri', function () {
-    return view('contents.suratundurdiri');
-});
-
-Route::get('/suratundurdiri2', function () {
-    return view('contents.suratundurdiri-2');
-});
-
-Route::get('/suratMahasiswa2', function () {
-    return view('contents.suratMahasiswa-2');
-});
-
-Route::get('/suratMahasiswa', function () {
-    return view('contents.suratMahasiswa');
-});
-
-Route::get('/kuesioner', function () {
-    return view('contents.kuesioner');
-});
-
-Route::get('/kuesioner-2', function () {
-    return view('contents.kuesioner-2');
-});
-
 // Profile Route
 Route::get('/biodata', [BiodataController::class, 'index']);
 Route::get('/biodata/edit', [BiodataController::class, 'edit']);
 Route::post('/biodata/update', [BiodataController::class, 'update']);
+
+// Mahasiswa Route
+Route::middleware(['auth', 'role:mahasiswa'])->group(function () {
+    // Dashboard Route
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+   
+    // Akademik Route
+    Route::get('/frs', function () {
+        return view('contents.frs');
+    });
+    
+    Route::get('/transkrip', function () {
+        return view('contents.transkrip');
+    });
+    
+    Route::get('/kurikulum', function () {
+        return view('contents.kurikulum');
+    });
+    
+    // Finansial Route
+    Route::get('/ukt', function () {
+        return view('contents.ukt');
+    });
+    
+    Route::get('/ukt-2', function () {
+        return view('contents.ukt-2');
+    });
+    
+    // Layanan Route
+    Route::get('/suratundurdiri', function () {
+        return view('contents.suratundurdiri');
+    });
+    
+    Route::get('/suratundurdiri2', function () {
+        return view('contents.suratundurdiri-2');
+    });
+    
+    Route::get('/suratMahasiswa2', function () {
+        return view('contents.suratMahasiswa-2');
+    });
+    
+    Route::get('/suratMahasiswa', function () {
+        return view('contents.suratMahasiswa');
+    });
+    
+    Route::get('/kuesioner', function () {
+        return view('contents.kuesioner');
+    });
+    
+    Route::get('/kuesioner-2', function () {
+        return view('contents.kuesioner-2');
+    });
+});
+
+// Dosen Route
+Route::middleware(['auth', 'role:dosen'])->group(function () {
+    // Dashboard Route
+    Route::get('/dashboard/dosen', [DashboardController::class, 'indexDosen'])->name('dashboard.dosen');
+});
