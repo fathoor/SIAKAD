675632ec97d290a20c61fb8f5ac9b22b5a3b5df8
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-24 07:11:40 +0700

    Added Contents
    Changes:
    + mataKuliah
    + kurikulum
    + kuesioner
    + daftarMahasiswa
    + nilaiMahasiswa
    + frs

---
+++ resources/views/contents/dosen/nilaiMahasiswa.blade.php
@@ -0,0 +1,19 @@
+@extends('dashboard')
+
+@section('title', 'Nilai Mahasiswa')
+
+{{-- Content --}}
+@section('main')
+
+<div class="container">
+    {{-- Content --}}
+    <main>
+        {{-- /view/contents/ --}}
+        <div class="content">
+            {{-- Isi disini --}}
+            
+        </div>
+    </main>
+</div>
+
+@endsection
