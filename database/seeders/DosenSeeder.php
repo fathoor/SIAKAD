af2880fb61d7325b023d3accd2cbe1b3ebd2dc43
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-05-26 07:21:29 +0700

    Updated AkunSeeder
    Changes:
    + Added more 'dosen' records
    + Updated several 'dosenKodeMK' values

--- database/seeders/DosenSeeder.php
+++ database/seeders/DosenSeeder.php
@@ -18,39 +18,63 @@ class DosenSeeder extends Seeder
         DB::table('dosen')->insert([
             [
                 'dosenNRP' => 5026201,
-                'dosenNama' => 'Radityo Prasetianto Wibowo',
+                'dosenNama' => 'Radityo Prasetianto Wibowo, S.Kom, M.Kom.',
                 'dosenKodeMK' => 'IS184412',
                 'isWali' => true
             ],
             [
                 'dosenNRP' => 5026202,
-                'dosenNama' => 'Mahendrawathi',
-                'dosenKodeMK' => 'IS184410',
-                'isWali' => true
-            ],
-            [
-                'dosenNRP' => 5026203,
-                'dosenNama' => 'Bekti Cahyo Hidayanto',
-                'dosenKodeMK' => 'IS184411',
-                'isWali' => true
-            ],
-            [
-                'dosenNRP' => 5026204,
-                'dosenNama' => 'Nisfu Asrul Sani',
-                'dosenKodeMK' => 'IS184413',
-                'isWali' => true
-            ],
-            [
-                'dosenNRP' => 5026205,
-                'dosenNama' => 'Faizal Johan Atletiko',
+                'dosenNama' => 'Mahendrawathi Er., ST., M.Sc., Ph.D.',
                 'dosenKodeMK' => 'IS184414',
                 'isWali' => true
             ],
             [
-                'dosenNRP' => 5026206,
-                'dosenNama' => 'Faizal Mahananto',
-                'dosenKodeMK' => 'IS184415',
+                'dosenNRP' => 5026203,
+                'dosenNama' => 'Bekti Cahyo Hidayanto, S.Si., M.Kom.',
+                'dosenKodeMK' => 'IS184309',
                 'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026204,
+                'dosenNama' => 'Ika Nurkasanah, S.Kom, M.Sc.',
+                'dosenKodeMK' => 'IS184310',
+                'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026205,
+                'dosenNama' => 'Faizal Johan Atletiko, S.Kom, MT.',
+                'dosenKodeMK' => 'IS184307',
+                'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026206,
+                'dosenNama' => 'Faizal Mahananto, S.Kom., M.Eng., Ph.D.',
+                'dosenKodeMK' => 'IW184301',
+                'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026207,
+                'dosenNama' => 'Retno Aulia Vinarti, S.Kom., M.Kom., Ph.D.',
+                'dosenKodeMK' => 'IS184101',
+                'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026208,
+                'dosenNama' => 'Ahmad Mukhlason, S.Kom., M.Sc., Ph.D.',
+                'dosenKodeMK' => 'IS184203',
+                'isWali' => true
+            ],
+            [
+                'dosenNRP' => 5026209,
+                'dosenNama' => 'Edwin Riksakomara, S.Kom., M.T.',
+                'dosenKodeMK' => 'IS184204',
+                'isWali' => false
+            ],
+            [
+                'dosenNRP' => 5026210,
+                'dosenNama' => 'Ir. Achmad Holil Noor Ali, M.Kom.',
+                'dosenKodeMK' => 'IS184308',
+                'isWali' => false
             ],
         ]);
     }
