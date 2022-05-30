20e4d8cc4f3490a2aefaa959950ba372de28cffd
@revonsio12 <revonsio12@gmail.com>
2022-05-30 18:21:07 +0700

    Update view-transkrip.blade.php
    mengedit tampilan

--- resources/views/contents/view-transkrip.blade.php
+++ resources/views/contents/view-transkrip.blade.php
@@ -18,9 +18,14 @@
                 </h2>
                 <table class="table table-borderless text-start">
                     <tr>
-                        <td>NRP/Nama</td>
+                        <td>Nama</td>
                         <td>:</td>
-                        <td>{{ auth()->user()->NRP }}/{{ auth()->user()->nama }}</td>
+                        <td>{{ auth()->user()->nama }}</td>
+                    </tr>
+                    <tr>
+                        <td>NRP</td>
+                        <td>:</td>
+                        <td>{{ auth()->user()->NRP }}</td>
                     </tr>
                     <tr>
                         <td>SKS Tempuh / SKS Normal</td>
@@ -31,30 +36,6 @@
                         <td>Status</td>
                         <td>:</td>
                         <td></td>
-                    </tr>
-                </table>
-                <table class="table table-borderless">
-                    <tr>
-                        <td>Format</td>
-                        <td>:</td>
-                        <td>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Pilih
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Webpage</a></li>
-                                    <li><a class="dropdown-item" href="#">Plain HTML</a></li>
-                                    <li><a class="dropdown-item" href="#">Microsoft Word Document</a></li>
-                                    <li><a class="dropdown-item" href="#">Microsoft Excel Spreadsheet</a></li>
-                                </ul>
-                            </div>
-                        </td>
-                        <td><button type="button" class="btn btn-primary">
-                                Cetak Transkrip
-                            </button>
-                        </td>
                     </tr>
                 </table>
             </div>
