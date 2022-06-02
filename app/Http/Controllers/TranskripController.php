ba187abda9adeacbb00f8fa14ba694d786bf53ae
Ellion Blessan <ellionblessan@gmail.com>
2022-06-02 13:26:35 +0700

    Update transkrip view
    Updated transkrip view to display user data

--- app/Http/Controllers/TranskripController.php
+++ app/Http/Controllers/TranskripController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TranskripController extends Controller
 {
     public function view(Request $request)
     {
-
-        return view('contents.view-transkrip');
+        $format = $request->input('format');
+        $mkSKS = DB::table('frs')
+            ->join('mata_kuliah', 'frs.kodeMK', '=', 'mata_kuliah.kodeMataKuliah')
+            ->where('frs.NRP', auth()->user()->NRP)
+            ->get();
+        $sksTempuh = $mkSKS->sum('sks');
+        $sksLulus = $mkSKS->where('nilai', '>', '0')->sum('sks');
+        switch($format) {
+            case '1':
+                return view('contents.view-transkrip', ['sksTempuh' => $sksTempuh, 'sksLulus' => $sksLulus]);
+                break;
+            case '2':
+                return view('contents.transkrip.excel');
+                break;
+            case '3':
+                return view('contents.transkrip.pdf');
+                break;
+            case '4':
+                return view('contents.transkrip.word');
+                break;
+        }
     }
 }
