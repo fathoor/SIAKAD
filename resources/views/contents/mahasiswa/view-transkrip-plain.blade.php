06add15223ca778ce68ed93d1117f9a2af363390
@revonsio12 <revonsio12@gmail.com>
2022-06-05 20:02:24 +0700

    Update view-transkrip-plain.blade.php
    edit tampilan

--- resources/views/contents/mahasiswa/view-transkrip-plain.blade.php
+++ resources/views/contents/mahasiswa/view-transkrip-plain.blade.php
@@ -5,70 +5,67 @@
         <h2>
             <strong> Transkrip Mata Kuliah </strong>
         </h2>
-        <table border="1">
+        <table border="1" cellspacing="0">
             <tr>
                 <td>Nama</td>
-                <td>:</td>
                 <td>{{ auth()->user()->nama }}</td>
             </tr>
             <tr>
                 <td>NRP</td>
-                <td>:</td>
                 <td>{{ auth()->user()->NRP }}</td>
             </tr>
             <tr>
                 <td>SKS Tempuh / SKS Lulus</td>
-                <td>:</td>
                 <td>{{ $sksTempuh }}/{{ $sksLulus }}</td>
             </tr>
             <tr>
                 <td>Status</td>
-                <td>:</td>
                 <td>Normal</td>
             </tr>
         </table>
-        `<br>
-        <table border="1">
-            <tr align="center">
-                <td colspan="5">
-                    <h3>Tahap: Persiapan</h3>
-                </td>
+        <br>
+        <br>
+        <table border="1" cellspacing="0">
+            <tr>
+                <th colspan="5">
+                    Tahap: Persiapan
+                </th>
             </tr>
             <tr>
                 <th width="150px">Kode</th>
                 <th width="300px">Mata Kuliah</th>
                 <th width="50px">SKS</th>
                 <th width="150px">Historis Nilai</th>
-                <th width="100px">Nilai</th>
-                @foreach ($mkPersiapan as $mkp)
-                    <?php
-                    $semester = $mkp->periode;
-                    $smt = explode(' ', $semester);
-                    if (86 <= $mkp->nilai) {
-                        $nilaiAngka = 'A';
-                    } elseif (76 <= $mkp->nilai && $mkp->nilai <= 85) {
-                        $nilaiAngka = 'AB';
-                    } elseif (66 <= $mkp->nilai && $mkp->nilai <= 75) {
-                        $nilaiAngka = 'B';
-                    } elseif (61 <= $mkp->nilai && $mkp->nilai <= 65) {
-                        $nilaiAngka = 'BC';
-                    } elseif (56 <= $mkp->nilai && $mkp->nilai <= 60) {
-                        $nilaiAngka = 'C';
-                    } elseif (41 <= $mkp->nilai && $mkp->nilai <= 55) {
-                        $nilaiAngka = 'D';
-                    } else {
-                        $nilaiAngka = 'E';
-                    }
-                    ?>
-            <tr>
-                <td>{{ $mkp->kodeMataKuliah }}</td>
-                <td>{{ $mkp->namaMataKuliah }}</td>
-                <td>{{ $mkp->sks }}</td>
-                <td>{{ $smt[0] . '/' . $smt[1] . '/' . $nilaiAngka }}</td>
-                <td>{{ $nilaiAngka }}</td>
+                <th width="100px">Nilai</td>
             </tr>
+            @foreach ($mkPersiapan as $mkp)
+                <?php
+                $semester = $mkp->periode;
+                $smt = explode(' ', $semester);
+                if (86 <= $mkp->nilai) {
+                    $nilaiAngka = 'A';
+                } elseif (76 <= $mkp->nilai && $mkp->nilai <= 85) {
+                    $nilaiAngka = 'AB';
+                } elseif (66 <= $mkp->nilai && $mkp->nilai <= 75) {
+                    $nilaiAngka = 'B';
+                } elseif (61 <= $mkp->nilai && $mkp->nilai <= 65) {
+                    $nilaiAngka = 'BC';
+                } elseif (56 <= $mkp->nilai && $mkp->nilai <= 60) {
+                    $nilaiAngka = 'C';
+                } elseif (41 <= $mkp->nilai && $mkp->nilai <= 55) {
+                    $nilaiAngka = 'D';
+                } else {
+                    $nilaiAngka = 'E';
+                }
+                ?>
+                <tr>
+                    <td>{{ $mkp->kodeMataKuliah }}</td>
+                    <td>{{ $mkp->namaMataKuliah }}</td>
+                    <td>{{ $mkp->sks }}</td>
+                    <td>{{ $smt[0] . '/' . $smt[1] . '/' . $nilaiAngka }}</td>
+                    <td>{{ $nilaiAngka }}</td>
+                </tr>
             @endforeach
-            </tr>
             <tr align="center">
                 <td colspan="5">Total Sks Tahap Persiapan: {{ $mkPersiapan->sum('sks') }}</td>
             </tr>
@@ -76,8 +73,14 @@
                 <td colspan="5">IP Tahap Persiapan: {{ $ipPersiapan }}</td>
             </tr>
         </table>
-        <h3>Tahap: Sarjana</h3>
-        <table border="1">
+        <br>
+        <br>
+        <table border="1" cellspacing="0">
+            <tr>
+                <th colspan="5">
+                    Tahap: Sarjana
+                </th>
+            </tr>
             <tr>
                 <th width="150px">Kode</th>
                 <th width="300px">Mata Kuliah</th>
@@ -113,33 +116,26 @@
                     <td>{{ $nilaiAngka }}</td>
                 </tr>
             @endforeach
-        </table>
-        <table>
-            <tr>
-                <td>Total Sks Tahap Sarjana</td>
-                <td>:</td>
-                <td>{{ $mkSarjana->sum('sks') }}</td>
+            <tr align="center">
+                <td colspan="5">Total Sks Tahap Sarjana: {{ $mkSarjana->sum('sks') }}</td>
             </tr>
-            <tr>
-                <td>IP Tahap Sarjana</td>
-                <td>:</td>
-                <td>{{ $ipSarjana }}</td>
+            <tr align="center">
+                <td colspan="5">IP Tahap Sarjana: {{ $ipSarjana }}</td>
             </tr>
         </table>
-        <table>
+        <br>
+        <br>
+        <table border="1" cellspacing="0">
             <tr>
                 <td>Total Sks</td>
-                <td>:</td>
                 <td>{{ $mkPersiapan->sum('sks') + $mkSarjana->sum('sks') }}</td>
             </tr>
             <tr>
                 <td>IPK</td>
-                <td>:</td>
                 <td>{{ $ipk }}</td>
             </tr>
         </table>
     </center>
-    </main>
 </body>
 
 </html>
